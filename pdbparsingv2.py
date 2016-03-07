49d892541520408c3dc5438ad91cfffe39ef4c0c
ariadna-ml <ariadna.marin.llaurado@gmail.com>
2016-03-07 22:17:34 +0100

    gets sequences from FASTA as input

--- pdbparsingv2.py
+++ pdbparsingv2.py
@@ -1,19 +1,26 @@
 from Bio.PDB import *
 import pylab
 import numpy as np
+import os.path as os
+from Bio import SeqIO
 
-if __name__=='__main__':
-	fhout=open('outputmatrix.txt','w')
-	#1X9L,prot simple de unha soa cadea.
+def parse_input(input_data):
+	if os.isfile(input_data)==True:
+		handle = open(input_data, "rU")
+		for record in SeqIO.parse(handle, "fasta") :
+			main_function1(record.id)
+		
+	else:
+		main_function1(input_data)
+		
+def main_function1(protein_name):
 	parser = PDBParser()
 	pdbl = PDBList() 
-	fh=pdbl.retrieve_pdb_file('2cd0') #downloads the pdb file of 2CD0 protein
-	structure = parser.get_structure('2cd0',fh) #extrae la inform estruct de la proteina descargada. 2cd0 es trivial.
-	counter=0
-
+	fh=pdbl.retrieve_pdb_file(protein_name) #downloads the pdb file of 2CD0 protein
+	structure = parser.get_structure(protein_name,fh) #extrae la inform estruct de la proteina descargada. 2cd0 es trivial.
 	mylist2=list()
+	
 	i=0
-
 	for residue in structure[0].get_residues(): # generator
 		tag1=residue.get_full_id()
 		mylist=list()
@@ -25,11 +32,8 @@ if __name__=='__main__':
 						mylist.append(residue['CA']-residue2['CA'])
 					except:
 						mylist.append(10000)
-			print(tag1)
 			mylist2.append(mylist)
 			i+=1
-	print i
-
 	distancem=np.asarray(mylist2)
 	'''
 	# Thresholded map
@@ -41,4 +45,4 @@ if __name__=='__main__':
 	# Colormap
 	pylab.matshow(np.transpose(distancem))
 	pylab.colorbar()
-	pylab.show()
+	pylab.savefig(protein_name + '.png')
