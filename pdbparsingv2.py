d0e26d95b212e8bb9e518cd7f228875498b9fe09
Mariona <mariona_tf@hotmail.com>
2016-03-09 09:22:36 +0100

    pdb varsing v2

--- pdbparsingv2.py
+++ pdbparsingv2.py
@@ -1,22 +1,30 @@
 from Bio.PDB import *
 import pylab
 import numpy as np
-import os.path as os
+import os
 from Bio import SeqIO
 
 def parse_input(input_data):
-	if os.isfile(input_data)==True:
-		handle = open(input_data, "rU")
-		for record in SeqIO.parse(handle, "fasta") :
-			main_function1(record.id)
+	if ('.fa' in input_data)==True:
+		handle = open(input_data,'r')
+		i=0
+		protein_name=list()
+		for record in SeqIO.parse(handle, "fasta"): 
+			pdb_name=(record.id).split("|")
+			protein_name.append(pdb_name[0])
+			i+=1
+		if i==1: # not really a list
+			protein_name=str(protein_name)
+		return protein_name
 		
-	else:
-		main_function1(input_data)
 		
-def main_function1(protein_name):
+def get_distancem(protein_name):
+	d = 'pdb_files'
+	if not os.path.exists(d):
+		os.makedirs(d)
 	parser = PDBParser()
 	pdbl = PDBList() 
-	fh=pdbl.retrieve_pdb_file(protein_name) #downloads the pdb file of 2CD0 protein
+	fh=pdbl.retrieve_pdb_file(protein_name,obsolete=0,pdir=d) #downloads the pdb file of 2CD0 protein
 	structure = parser.get_structure(protein_name,fh) #extrae la inform estruct de la proteina descargada. 2cd0 es trivial.
 	mylist2=list()
 	
@@ -35,14 +43,17 @@ def main_function1(protein_name):
 			mylist2.append(mylist)
 			i+=1
 	distancem=np.asarray(mylist2)
-	'''
-	# Thresholded map
-	contact_map = distancem > 15    
-	pylab.autumn()
-	pylab.imshow(np.transpose(contact_map))
-	pylab.show()
-	'''
-	# Colormap
-	pylab.matshow(np.transpose(distancem))
-	pylab.colorbar()
-	pylab.savefig(protein_name + '.png')
+	return distancem
+
+def contactm(distancem,threshold=None):
+	if threshold == None:
+		# Colormap
+		pylab.matshow(np.transpose(distancem))
+		pylab.colorbar()
+		pylab.savefig(protein_name + '.png')
+	else:
+		# Thresholded map
+		contact_map = distancem > 15    
+		pylab.autumn()
+		pylab.imshow(np.transpose(contact_map))
+		pylab.savefig(protein_name + '_thr.png')
